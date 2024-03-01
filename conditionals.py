8ffc6b70bd457149bd24bbb376b5f1e4338d162e
Dengkar <03230370.gcbs@rub.edu.bt>
2024-03-01 12:56:35 +0600

    conditionals and identation

---
+++ conditionals.py
@@ -0,0 +1,34 @@
+#caluculator appilications made using 
+#variable and if statement
+#1. get input from the user 
+#2 do calculation based on user input 
+#3 give output to the user 
+print('above user input------------')
+userinput = ''
+print('above whatUsertyped------------')
+whatUsertyped = input()
+print('youtype')
+print(whatUsertyped)
+print('done with the program-------')
+#1 get input from user
+#2 do calculation based on user typed)
+#2.1
+
+
+print('* for multplication')
+print('+ for additional')
+print('- for subtractions ')
+print('/ for division')
+print('Usertyped:, whatUsertyped')
+print('User input-typed', type(whatUsertyped))
+if whatUsertyped == "+":
+    print('doing addition')
+    print('doing more addition')
+    if 'a' == 'b':
+        print('a is not b')
+    if 'b'== 'b':
+        print('b is b') 
+if whatUsertyped == "-":
+    print('doing subtraction')
+    print('doing more substaction')
+
